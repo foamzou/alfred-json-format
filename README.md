afa1eac510afc3c519e24543e8443463830b5c88
zoufeng <zoufeng@ihuayue.cn>
2019-05-24 17:15:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Json-format
+
+从系统剪贴板读取字符串，并格式美化json
+
+## 使用方法
+* 复制待格式化字符串
+* 按组合键`command+j`触发打开网页。（组合键可自行修改）
+
+<img src="http://file.zoufeng.net/ChtgcVznXdqAaVgVAAD6sL6KEms770.png">
---
+++ src/index.php
@@ -0,0 +1,89 @@
+<?php
+
+$input = exec('pbpaste');
+
+function _format_json($json, $html = false) {
+		$tabcount = 0; 
+		$result = ''; 
+		$inquote = false; 
+		$ignorenext = false; 
+		if ($html) { 
+		    $tab = "&nbsp;&nbsp;&nbsp;"; 
+		    $newline = "<br/>"; 
+		} else { 
+		    $tab = "\t"; 
+		    $newline = "\n"; 
+		} 
+		for($i = 0; $i < strlen($json); $i++) { 
+		    $char = $json[$i]; 
+		    if ($ignorenext) { 
+		        $result .= $char; 
+		        $ignorenext = false; 
+		    } else { 
+		        switch($char) { 
+		            case '{': 
+		                $tabcount++; 
+		                $result .= $char . $newline . str_repeat($tab, $tabcount); 
+		                break; 
+		            case '}': 
+		                $tabcount--; 
+		                $result = trim($result) . $newline . str_repeat($tab, $tabcount) . $char; 
+		                break; 
+		            case ',': 
+		                $result .= $char . $newline . str_repeat($tab, $tabcount); 
+		                break; 
+		            case '"': 
+		                $inquote = !$inquote; 
+		                $result .= $char; 
+		                break; 
+		            case '\\': 
+		                if ($inquote) $ignorenext = true; 
+		                $result .= $char; 
+		                break; 
+		            default: 
+		                $result .= $char; 
+		        } 
+		    } 
+		} 
+		return $result; 
+	}
+
+$input = json_encode(json_decode($input, true), 256);
+
+$ret = _format_json($input);
+
+$style = "<style> textarea {
+    border: none;
+    overflow: hidden;
+    outline: none;
+	width: 100%;
+	overflow: hidden;
+
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+	color: black;
+	font-size: 15px;
+
+    resize: none; /*remove the resize handle on the bottom right*/
+}
+</style>";
+
+$script = "<script>
+var element = document.getElementById('t');
+	console.log('dddd')
+	console.log(element.scrollHeight)
+    element.style.height = '5px';
+    element.style.height = (element.scrollHeight)+'px';
+
+</script>
+";
+
+$ret = $style . "<textarea id='t' disabled='disabled'>{$ret}</textarea>" . $script;
+
+$filename = '/tmp/json_' . time(). '.html';
+file_put_contents($filename, $ret);
+exec("open {$filename}");
+
+
+?>
